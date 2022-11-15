44cee1fb64fce80b83803fb8339e5ff99bd27d12
MahmoudAbdElrdy <47526228+MahmoudAbdElrdy@users.noreply.github.com>
2022-11-15 06:49:09 +0200

    OfficeId

---
+++ Maintenance.Infrastructure/Migrations/20221115044854_OfficeId.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Maintenance.Infrastructure.Migrations
+{
+    public partial class OfficeId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RegionId",
+                table: "RequestComplanit",
+                newName: "OfficeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OfficeId",
+                table: "RequestComplanit",
+                newName: "RegionId");
+        }
+    }
+}
