b8586ecb7ab4a16d19cb8b922b47e8e810cb354c
Mahmoudattiashgardi <mahmoudattia@shgardi.app>
2022-11-23 13:15:16 +0200

    codesms

--- Maintenance.Domain/Entities/Complanits/RequestComplanit.cs
+++ Maintenance.Domain/Entities/Complanits/RequestComplanit.cs
@@ -25,8 +25,8 @@ namespace Maintenance.Domain.Entities.Complanits
         //public long? OfficeId { get; set; }
        // public long? RegionId { get; set; } 
         public string SerialNumber { get; set; } 
-        public string Code { get; set; }  
-        public string CodeSms { get; set; }   
+        public string? Code { get; set; }  
+        public string? CodeSms { get; set; }   
         public virtual ICollection<CheckListRequest> CheckListRequests { get; set; } = new List<CheckListRequest>();
         public virtual ICollection<AttachmentComplanit> AttachmentsComplanit { get; set; } = new List<AttachmentComplanit>();
         public virtual ICollection<ComplanitHistory> ComplanitHistory { get; set; } = new List<ComplanitHistory>();
